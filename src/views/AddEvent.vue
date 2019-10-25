0e86574038158636a11723277800ce08e20a1202
Bowen Yin <bowenvictoryin@gmail.com>
2019-10-24 20:04:10 -0700

    Fix up everything

---
+++ src/views/AddEvent.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-text-field v-model="event.name"></v-text-field>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      event: {
+        name: "",
+        category: "",
+        start: null,
+        end: null
+      }
+    };
+  },
+  methods: {
+    submit() {
+      return fetch("https://bell.dev.harker.org/admin/addEvents", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          access_token: this.accessToken,
+          date: this.date, // ISO!!!
+          clear_all: false,
+          events: [this.event],
+        }),
+      }).then(response => {
+        
+      }).catch(error => {
+        
+      });
+    }
+  },
+};
+</script>
